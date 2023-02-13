07ca341e1b4c78794c66e426e0ed1d54d40bf9bf
abubaker-cs <mohammad_abubaker@yahoo.com>
2023-02-14 00:14:30 +0500

    Completed: Comments

--- comments.py
+++ comments.py
@@ -1,2 +1,17 @@
 # Python Comments
 print("To use the comments, just add # before the line")
+
+# Comments can be placed at the end of a line, and Python will ignore the rest of the line:
+print("The comment will be added at the end of this line")  # Like this
+
+# A comment does not have to be text that explains the code, it can also be used to prevent Python from executing code:
+# print("Hello, World!")
+
+# Python does not really have a syntax for multiline comments.
+
+# This
+# will
+# be a
+# multiline
+# comment
+
