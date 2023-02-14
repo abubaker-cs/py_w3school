a3ab04c19d186b1bd824d53f8b8da95c66a0998c
abubaker-cs <mohammad_abubaker@yahoo.com>
2023-02-14 11:15:18 +0500

    Studying: Numbers

---
+++ numbers.py
@@ -0,0 +1,52 @@
+"""
+    Python Numbers
+    1. Integer
+    2. Float
+    3. Complex
+"""
+
+x = 1  # int
+y = 2.8  # float
+z = 1j  # complex
+
+# type() = to verify the type of existing variable
+print("1: " + str(type(x)))
+print("2.8: " + str(type(y)))
+print("1j: " + str(type(z)))
+
+print("")
+print("-------------------------------------------")
+print("")
+
+# int = Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+x = 1
+y = 2342349082398493284
+z = -234234
+print("1: " + str(type(x)))
+print("2342349082398493284: " + str(type(y)))
+print("-234234: " + str(type(z)))
+
+print("")
+print("-------------------------------------------")
+print("")
+
+# float = Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+x = 123.3434
+y = 1.0
+z = -3434.343
+print("123.3434: " + str(type(x)))
+print("1.0: " + str(type(y)))
+print("-3434.343: " + str(type(z)))
+
+print("")
+print("-------------------------------------------")
+print("")
+
+# Note: Float can also be scientific numbers with an "e" to indicate the power of 10.
+x = 34e4
+y = 12E3
+z = -94.3e100
+print("34e4: " + str(type(x)))
+print("12E3: " + str(type(y)))
+print("-94.3e100: " + str(type(z)))
+
