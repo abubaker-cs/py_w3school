295bb93962456e87583cfc82da6253d28eec3c28
abubaker-cs <mohammad_abubaker@yahoo.com>
2023-02-14 10:11:03 +0500

    Completed: 15 Data Types (8 Categories)

--- data_types.py
+++ data_types.py
@@ -98,3 +98,44 @@ print("NoneType: " + str(x))
     ==============================
     If you want to specify the data type, you can use the following constructor functions:
 """
+x = str("Hello World")
+print(type(x))
+
+x = int(29)
+print(type(x))
+
+x = float(32.32)
+print(type(x))
+
+x = complex(1j)
+print(type(x))
+
+x = list(("apple", "banana", "cherry"))
+print(type(x))
+
+x = tuple(("apple", "banana", "cherry"))
+print(type(x))
+
+x = range(6)
+print(type(x))
+
+x = dict(name="John", age=36)
+print(type(x))
+
+x = set(("apple", "banana", "cherry"))
+print(type(x))
+
+x = frozenset(("apple", "banana", "cherry"))
+print(type(x))
+
+x = bool(5)
+print(type(x))
+
+x = bytes(5)
+print(type(x))
+
+x = bytearray(5)
+print(type(x))
+
+x = memoryview(bytes(5))
+print(type(x))
