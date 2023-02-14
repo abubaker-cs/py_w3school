3ef6a5b2be67b10a3da14c0813594df7902a7097
abubaker-cs <mohammad_abubaker@yahoo.com>
2023-02-14 13:13:24 +0500

    Next: Strings & 44 Methods

---
+++ strings.py
@@ -0,0 +1,78 @@
+"""
+    Understanding using Strings in Python
+"""
+
+"""
+    01 Python Strings
+"""
+
+# Strings
+
+# Assign String to a Variable
+
+# Multiline Strings
+
+# Strings are Arrays
+
+# Looping Through a String
+
+# String Length
+
+# Check String
+
+# Check if NOT
+
+"""
+    02 Slicing Strings 
+"""
+
+# Slicing
+
+# Slice From the Start
+
+# Slice To the End
+
+# Negative Indexing
+
+"""
+    03 Modifying Strings 
+"""
+
+# Upper Case
+
+# Lower Case
+
+# Remove Whitespace
+
+# Replace String
+
+# Split String
+
+# String Methods
+
+"""
+    04 Concatenate Strings 
+"""
+
+# String Concatenation
+
+"""
+    05 Format Strings 
+"""
+
+# String Format
+
+
+"""
+    06 Escape Characters 
+"""
+
+# Escape Character
+
+# Escape Characters
+
+"""
+    07 String Methods (44)
+"""
+
+# String Methods (44)
